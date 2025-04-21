9e80fb34e889b29e54fc89bab5aa6d127dc378e5
Diptalok <91647912+Diptalok@users.noreply.github.com>
2025-04-21 08:04:51 +0000

    JS Note

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,13 @@
+const id = 15
+let mail = "s@s.com"
+var pass = "123re"
+country = "rrr"
+
+country = "india"
+pass = 1234
+mail = "x@x"
+//id = 20
+
+console.log(id,country,pass,mail,id)
+
+console.table([country,pass,mail,id])
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,13 @@
+console.log(3+3)
+
+let name = "Miku"
+let age = 13
+let isLoggedIn = true
+let tree;
+let water = null
+
+console.log(name,age,isLoggedIn,tree,water);
+
+console.log(typeof tree,water);
+
+console.log(typeof name,age,isLoggedIn)
