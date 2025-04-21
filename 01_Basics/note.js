c262b492a681888f25d1a361d95db959f0aaf7c4
Diptalok <91647912+Diptalok@users.noreply.github.com>
2025-04-21 08:06:29 +0000

    change

---
+++ 01_Basics/note.js
@@ -0,0 +1,20 @@
+/*
+add a folder -> add a fie under the folder -> write code -> open terminal in view -> node filename (enter)
+download latest node js from nodejs.org and install it -> check it in terminal node -v
+compiler interpreter sw have the syntax structure reading capability
+we can inject the js files in to the engine and execute the file in javascript
+node.js is a sw/execution engines which is downloaded and set up inside the editor VS and execute .js files
+code editor advantage - colour editing, colour console, colour correction, intendation 
+tree parser is a hirarchical node structure function used to read code and build a tree structure or code structre format
+inside github -> in repo-> code -> view - command pallete -> container -> node js with javascript -> create folder -> same process of file execution
+after writing code -> source control -> + -> commit -> push
+codespace -> 3dot -> delete - close the engine
+{} -> block scope
+if no value is assigned to a variable, it is defined as undefined
+for string "" required, but for int,float not required
+; is optional
+dont use var, use let and const is not editable
+"use strict" - engine will consider all the code in new standard version of js
+ECMA - standard community
+datattypes in js => number - 2>53, bigint, String - "", boolean-T/F, null - standalone value, undefined, symbol - unique  
+*/
