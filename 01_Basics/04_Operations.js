8a1923722cd0daf62f37b7d6a31fdf71de475d7b
Diptalok <91647912+Diptalok@users.noreply.github.com>
2025-04-27 10:07:25 +0000

    changes

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,10 @@
+let value = 3
+let value1 = -value
+console.log(value , value1); 
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2**2);
+console.log(2%2);
+console.log("string 1 "+"string 2");
